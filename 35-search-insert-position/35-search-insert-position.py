a35d469addfbf362bd439dcc749deb97c969f179
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2022-04-25 19:13:17 -0500

    Time: 80 ms (29.33%), Space: 14.8 MB (7.21%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -5,7 +5,7 @@ class Solution:
             mid=(l+r)//2
             if nums[mid]== target:
                 return mid
-            if nums[mid] < target:
+            elif nums[mid] < target:
                 l = mid+1
             else:
                 r = mid-1
