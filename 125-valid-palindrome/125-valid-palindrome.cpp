a1956a90b6bd395a9c676c69d9642d940fc64c2c
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2022-06-12 16:15:38 -0500

    Time: 462 ms (5.01%), Space: 7.1 MB (98.28%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = s.length();
+        int b = 0, e = l-1;
+        
+        while(b < e){
+            
+            if(isupper(s[b]) || isupper(s[e])){
+                s[b] = tolower(s[b]);
+                s[e] = tolower(s[e]);
+                continue;
+            }
+            else if( !isalnum(s[b])){
+                // cout << "entered"<<"\n";
+                s.erase(b,1);
+                e--;
+                continue;
+            }
+            else if( !isalnum(s[e])){
+                // cout << "entered"<<"\n";
+                s.erase(e,1);
+                e--;
+                continue;
+            }
+            
+            if(s[b] != s[e]){return false;}
+
+            b++;
+            e--;
+            
+        }
+        return true;
+    }
+};
