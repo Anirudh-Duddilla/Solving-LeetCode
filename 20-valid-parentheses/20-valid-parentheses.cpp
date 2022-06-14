7e1318f4135e4cac673f0a40aade8284a071c567
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2022-06-13 19:28:11 -0500

    Time: 0 ms (100.00%), Space: 6.5 MB (10.22%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std :: stack < char > stk;
+        for( char c:s){
+            if( (c == ')' || c == ']' || c == '}') && stk.empty()){
+                return false;
+            }
+            else{
+                switch(c){
+                    case '}':
+                        if(stk.top() != '{'){return false;}
+                        else{stk.pop(); continue;}
+                    case ')':
+                        if(stk.top() != '('){return false;}
+                        else{stk.pop(); continue;}
+                    case ']':
+                        if(stk.top() != '['){return false;}
+                        else{stk.pop(); continue;}
+                }
+            }
+            stk.push(c);
+        }
+        if(stk.empty()){return true;}
+        else{return false;}
+    }
+};
