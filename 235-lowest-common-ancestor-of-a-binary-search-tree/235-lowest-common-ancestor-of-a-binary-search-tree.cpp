b0ee8df864955667ebe57905da0fb1d0424f7628
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2022-06-26 17:09:05 -0500

    Time: 42 ms (55.02%), Space: 23.3 MB (63.73%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -13,29 +13,29 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
        
         // cout<<(p->val>root->val<q->val);
-        // if(p->val<root->val && root->val>q->val){
-        //     cout<<"end"<<endl;
-        //     return lowestCommonAncestor(root->left,p,q);
-        // }
-        // if(p->val>root->val &&root->val<q->val){
-        //     cout<<"chk"<<endl;
-        //     return lowestCommonAncestor(root->right,p,q);
-        // }
-        // if(p->val<root->val<q->val){
-        //     cout<<"ok"<<endl;
-        //     return root;
-        // }
-        // return root;
-        TreeNode* cur = root;
-        while (cur){
-            if(p->val < cur->val && cur -> val > q->val){
-                cur = cur ->left;
-            }
-            else if(p->val > cur->val && cur -> val < q->val){
-                cur = cur->right;
-            }
-            else {return cur;}
+        if(p->val<root->val && root->val>q->val){
+            // cout<<"end"<<endl;
+            return lowestCommonAncestor(root->left,p,q);
         }
-        return cur;
+        if(p->val>root->val &&root->val<q->val){
+            // cout<<"chk"<<endl;
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        if(p->val<root->val<q->val){
+            // cout<<"ok"<<endl;
+            return root;
+        }
+        return root;
+        // TreeNode* cur = root;
+        // while (cur){
+        //     if(p->val < cur->val && cur -> val > q->val){
+        //         cur = cur ->left;
+        //     }
+        //     else if(p->val > cur->val && cur -> val < q->val){
+        //         cur = cur->right;
+        //     }
+        //     else {return cur;}
+        // }
+        // return cur;
     }
 };
