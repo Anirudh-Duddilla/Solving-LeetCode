78e5c0b72b0a68d9e2e98e44d81598a15dbcfbc0
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2022-06-30 20:37:51 -0500

    Time: 43 ms (6.60%), Space: 13.9 MB (24.43%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         // if(!nums.size()){return vector<int> result;}
-        std::map<int, int> nums_map;
+        std::map<int, int> nums_map;    //Creating a map with integers of nums as key and values as number of time they repeated
         for(int i=0; i<nums.size(); i++){
             if(!nums_map.count(nums[i])){
-                nums_map[nums[i]] = 1;
+                nums_map[nums[i]] = 1;  
             }
             else{
                 nums_map[nums[i]]+=1;
@@ -16,11 +16,11 @@ public:
         for(auto &it: nums_map){
             A.push_back(it);
         }
-        sort(A.begin(), A.end(), cmp);
+        sort(A.begin(), A.end(), cmp);  //sorting the hash map with the values in descending order and storing each key value as pair of vectors
         std::vector<int> result;
         int j = 0;
         for(auto& it:A){
-            if(j>=k){cout << j<<endl;return result;}
+            if(j>=k){cout << j<<endl;return result;} //iterating through the pair vector until we check for k repeated elements
             cout <<A[j].first<<", ";
             result.push_back(it.first);
             j+=1;
