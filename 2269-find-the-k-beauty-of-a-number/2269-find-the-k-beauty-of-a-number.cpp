16ddb7dfee071eca1634ec69a2bfda3ad446c047
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2022-08-03 18:23:10 -0500

    Time: 0 ms (100.00%), Space: 6.3 MB (9.85%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
@@ -7,21 +7,13 @@ public:
         int k_num = 0;
         while(r < s.size()){
             if(r <= k-1){
-                // cout << "s[r]"<<s[r]<<endl;
                 k_num = k_num + ((s[r] - '0')*pow(10,(k-r-1)));
-                cout << "k_num " << k_num << endl;
-                cout << "r " << r <<endl;
                 if(num%k_num == 0 && r-l == k-1){cout << "if " << endl;div+=1;}
                 r+=1;
                 continue;
             }
-            // r+=1;
-            cout <<"r " << r <<endl;
             k_num  = ((k_num*10) + (s[r] - '0'));
-            cout << "k_num " << k_num << endl;
             k_num = k_num % lround(pow(10,k));            
-            cout << "k_num " << k_num << endl;
-            // cout <<"r " << r <<endl;
             if(k_num !=0 && num%k_num == 0){cout << "entrd"<<endl;div+=1;}
             r += 1;
             l += 1;
