695a0643008fc6cf92fbf8115d620e0b98179aa5
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2022-08-03 22:08:28 -0500

    Time: 362 ms (23.56%), Space: 80.5 MB (12.08%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -5,19 +5,13 @@ public:
         int p = 0;
         while(p < nums.size()){
             if(!id_map.count(nums[p])){id_map[nums[p]] = p;}
-            // cout << "p " <<p << endl;
-            // cout << "m_val " << id_map[nums[p]]<<endl;
-            // cout << (0 < abs(id_map[nums[p]] - p) && abs(id_map[nums[p]] - p) <= k) << endl;
             if(0 < abs(id_map[nums[p]] - p) && abs(id_map[nums[p]] - p) <= k){
                 return true;
             }
             else{
-                // cout << "dif " << abs(id_map[nums[p]] - p) << endl;
-                // cout << "m_key " << nums[p] << endl;
-                // cout << "m_val " << id_map[nums[p]]<<endl;
                 id_map[nums[p]] = p;
+                p += 1;
             }
-            p += 1;
         }
         return false;
     }
