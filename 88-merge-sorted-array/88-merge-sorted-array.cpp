bfab0158e23d5b80c6bbb56678c50714f5aec1d7
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2022-08-12 11:48:42 -0500

    Time: 4 ms (53.48%), Space: 9.1 MB (72.00%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,25 +3,16 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         if(!m){nums1 = nums2; n = -1;}
         int l = m+n - 1;
-        // m -=1;n-=1;
         while(n>0 ){
-            // cout << "m: " << nums1[m-1]<< " n: " << nums2[n-1]<< " l: " << nums1[l] << endl;
-            // cout << nums1[l]<<endl;
             if(nums1[m-1] <= nums2[n-1]){
-                cout <<"entrd if"<<endl;
                 nums1[l] = nums2[n-1];
-                cout << "m: " << nums1[m-1]<< " n: " << nums2[n-1]<< " l: " << nums1[l] << endl;
                 n-=1;
             }
             else{
-                cout <<"entrd el"<<endl;
                 nums1[l] = nums1[m-1];
                 nums1[m-1] = nums2[n-1];   
-                cout << "m: " << nums1[m-1]<< " n: " << nums2[n-1]<< " l: " << nums1[l] << endl;
                 if(m == 1){n-=1;l-=1;continue;}
                 m-=1;
-                // n-=1;
-
             }
             l-=1;
         }
