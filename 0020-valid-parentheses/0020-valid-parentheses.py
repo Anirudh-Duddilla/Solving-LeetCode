e5aab8288cf073b9b9a776766c8b1b35c42790b5
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-11-05 11:47:58 -0800

    Time: 0 ms (100.00%), Space: 16.8 MB (9.49%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,26 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stck = []
-        # stck.append(s[0])
         for c in s:
             if (c is ')' or c is ']' or c is '}') and len(stck)==0:
-                # print(len(stck), stck)
                 return False
             if c is ')':
-                # print(c, stck[-1])
                 if stck[-1] is not '(':return False
                 stck.pop()
             elif c is '}':
-                # print(c, stck[-1])
                 if stck[-1] is not '{':return False
                 stck.pop()
             elif c is ']':
-                # print(c, stck[-1])
                 if stck[-1] is not '[':return False
                 stck.pop()
             else:
                 stck.append(c)
-            # print(c, stck, stck[-1])
             
         return len(stck)==0
                 
