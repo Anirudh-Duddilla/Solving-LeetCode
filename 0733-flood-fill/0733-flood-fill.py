7a160d906ee3316c7cbe6a5a6b915d43955c03c5
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-11-10 19:34:04 -0800

    Time: 3 ms (18.05%), Space: 17 MB (7.44%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,27 +2,23 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         rows, cols = len(image), len(image[0])
         visit = set()
-        srcolor = image[sr][sc]
-        # visit.add((sr,sc))
-        image[sr][sc] = color
+        # srcolor = image[sr][sc]
+        # image[sr][sc] = color
         
         def dfs(row,col):
             if (row,col) in visit:return
             visit.add((row,col))
-            # print(row,col)
             dirs = [[0,1],[0,-1],[1,0],[-1,0]]
 
             for dr, dc in dirs:
                 r, c = row+dr, col+dc
-                # print("r ",r,"c ",c,"i ",image[r][c],"col ",color)
                 if (r in range(rows) and
                     c in range(cols) and
                     (r,c) not in visit and
-                    image[r][c] == srcolor):
+                    image[r][c] == image[sr][sc]):
                     image[r][c] = color
-                    # visit.add((r,c))
-                    # print("e: ",r,c,image[r][c],color)
                     dfs(r,c)
         dfs(sr,sc)
+        image[sr][sc] = color
         
         return image
