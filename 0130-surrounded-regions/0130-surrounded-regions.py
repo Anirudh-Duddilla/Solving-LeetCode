1fde2ae4e2b594a16777764ece0288ce539c3b92
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-11-14 11:20:21 -0800

    Time: 7 ms (63.32%), Space: 20.9 MB (30.84%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,63 +1,30 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        visit = set()
-        rows, cols = len(board), len(board[0])
-        q = deque()
-        borderls = []
+        ROWS, COLS = len(board), len(board[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        def capture():
+            q = deque()
+            for r in range(ROWS):
+                for c in range(COLS):
+                    if (r == 0 or r == ROWS - 1 or 
+                        c == 0 or c == COLS - 1 and 
+                        board[r][c] == "O"
+                    ):
+                        q.append((r, c))
+            while q:
+                r, c = q.popleft()
+                if board[r][c] == "O":
+                    board[r][c] = "T"
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if 0 <= nr < ROWS and 0 <= nc < COLS:
+                            q.append((nr, nc))
         
-        ls = list(itertools.product(list(range(1,rows-1)), list(range(1,cols-1))))
-        for r in range(0,rows):
-            for c in range(0,cols):
-                if ((r,c) not in ls and
-                    board[r][c] == 'O'):
-                    q.append((r,c))
-                    borderls.append((r,c))
-                    visit.add((r,c))
-        
-        dirs = [[1,0],[0,1],[-1,0],[0,-1]]
-        # print(borderls, q)
-        while q:
-            row, col = q.popleft()
-            
-            for dr, dc in dirs:
-                r, c = row+dr, col+dc
-                if ( r in range(rows) and
-                   c in range(cols) and
-                   (r,c) not in visit and
-                   board[r][c] == 'O'):
-                    visit.add((r,c))
-                    borderls.append((r,c))
-                    q.append((r,c))
-        # print(borderls)
-        # print(q)
-        def bfs(row,col):
-            if (row, col) in visit: return
-            visit.add((row,col))
-            # print(row,col)
-            for dr, dc in dirs:
-                r, c = row+dr, col+dc
-                # print((r,c) not in visit,board[r][c] == 'O',(r,c) not in borderls)
-                if ( r in range(rows) and
-                   c in range(cols) and
-                   (r,c) not in visit and
-                   board[r][c] == 'O' and
-                   (r,c) not in borderls):
-                    # print((r,c))
-                    board[r][c] = 'X'
-                    # print(board[r][c])
-                    bfs(r,c)
-        
-        for r, c in ls:
-            if board[r][c] == 'O' and (r,c) not in visit and (r,c) not in borderls:
-                    board[r][c] = 'X'
-                    bfs(r,c)
-        # for r in range(0,rows):
-        #     for c in range(0,cols):
-        #         if board[r][c] == 'O' and (r,c) not in visit and not in borderls:
-        #             board[r][c] = 'X'
-        #             bfs(r,c)
-                    
-        # print(visit)
+        capture()
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
