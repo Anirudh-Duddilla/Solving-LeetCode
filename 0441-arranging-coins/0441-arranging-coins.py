757887d1322f71468e96ee2fc705642d3eabe546
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-11-20 09:24:06 -0800

    Time: 1 ms (75.75%), Space: 16.8 MB (5.97%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -2,18 +2,13 @@ class Solution:
     def arrangeCoins(self, n: int) -> int:
         l, r = 1, n
         res = 0
-        def formula(m):
-            # print(m,int((m*(m+1))/2))
-            return round((m*(m+1))/2)
         
         while l <= r:
             mid = (l+r)//2
-            f = formula(mid)
-            # print("l",l,"m",mid,"f",f,"r",r,"n",n)
+            f = round((mid*(mid+1))/2)
             if f > n:
                 r = mid - 1
             else:
                 l = mid + 1
                 res = max(res, mid)
-                # return mid
         return res
