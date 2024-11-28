5b774403ed147415974ba38bf9ca6b93fa73f676
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-11-27 22:48:13 -0800

    Time: 2843 ms (5.00%), Space: 21.1 MB (6.86%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -5,10 +5,14 @@ class NumArray:
 
     def sumRange(self, left: int, right: int) -> int:
         sum = 0
-        for i in range(left,right+1):
-            # print(i,self.nums_array[i])
-            sum += self.nums_array[i]
-            
+        while left <= right:
+            # print(left,self.nums_array[left],right,self.nums_array[right],sum)
+            if left == right:
+                sum = sum + self.nums_array[left]
+                return sum
+            sum = sum + self.nums_array[left] + self.nums_array[right]
+            left += 1
+            right -= 1
         return sum
 
 
