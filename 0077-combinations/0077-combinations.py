92d7165200c8ff6679a8af2794715548fef001a5
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-12-04 15:49:39 -0800

    Time: 147 ms (32.92%), Space: 59.1 MB (38.99%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -3,18 +3,19 @@ class Solution:
         res = []
         
         def dfs(i, s):
+            # If the combination is of size k, add it to the result
             if len(s) == k:
-                res.append(s[:])
+                res.append(s[:])  # Append a copy of the current combination
                 return
-            if i > n or len(s) > k:
+            # If we've used all elements or reached the limit, stop recursion
+            if i > n:
                 return
-            else:
-                s.append(i)
-                dfs(i+1,s)
-                s.pop()
-                dfs(i+1,s)
-                
-            return res
-        dfs(1,[])
-        
+            # Include the current number and recurse with the next number
+            s.append(i)
+            dfs(i + 1, s)
+            s.pop()  # Backtrack, remove the last number
+            # Do not include the current number and recurse with the next number
+            dfs(i + 1, s)
+        # Start DFS from number 1
+        dfs(1, [])
         return res
