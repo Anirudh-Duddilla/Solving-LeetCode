75a6b9ce7316420caf702087c13195c7654ad0ca
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-12-05 13:31:20 -0800

    Time: 0 ms (100.00%), Space: 17.4 MB (5.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        res_list = [0] * 2
-        res_list[0] = 1
-        res_list[1] = 1
-        total = res_list[1]
+        a, b = 1, 1
         for i in range(2,n+1):
-            total = res_list[0] + res_list[1]
-            res_list[0],res_list[1] = res_list[1],total
-        return total
+            total = a + b
+            a, b = b, total
+        return b
