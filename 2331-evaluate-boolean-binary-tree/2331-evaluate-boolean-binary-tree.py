35201dd8183872b64e24cad26905a4ce188c0aee
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-12-06 16:34:19 -0800

    Time: 0 ms (100.00%), Space: 17.6 MB (11.34%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.py
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.py
@@ -6,20 +6,12 @@
 #         self.right = right
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
-        # res = 1
         def dfs(curr):
             if not curr.left and not curr.right:
                 return bool(curr.val)
-            # print("strt",curr.val)
-            a = dfs(curr.left)
-            b = dfs(curr.right)
-            # print(curr.left.val,curr.val,curr.right.val)
-            
             if curr.val == 2:
-                # print("or",a,b,curr.left.val or curr.right.val)
-                return a or b
+                return dfs(curr.left) or dfs(curr.right)
             elif curr.val == 3:
-                # print("and",a,b,curr.left.val and curr.right.val)
-                return a and b
+                return dfs(curr.left) and dfs(curr.right)
             
         return dfs(root)
