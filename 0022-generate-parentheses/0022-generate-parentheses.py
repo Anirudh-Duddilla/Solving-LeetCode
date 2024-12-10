913f689c3b1c9226e4cf0b305ce639b8598621f7
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-12-10 07:57:37 -0800

    Time: 3 ms (34.97%), Space: 17.5 MB (9.87%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -4,22 +4,17 @@ class Solution:
         res = []
         
         def dfs(openn,close):
-            # print(openn,close,stack)
             if openn == close == n:
                 res.append("".join(stack))
                 return
             if openn<n:
                 stack.append("(")
                 dfs(openn+1,close)
-                # print("o",stack)
                 stack.pop()
-                # print("o",stack)
             if close < openn:
                 stack.append(")")
                 dfs(openn,close+1)
-                # print("c",stack)
                 stack.pop()
-                # print("c",stack)
 
         dfs(0,0)       
         
