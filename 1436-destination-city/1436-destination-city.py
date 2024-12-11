1367458eb8af53b9b770095b714de09bb8589c47
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-12-11 12:37:49 -0800

    Time: 0 ms (100.00%), Space: 17.2 MB (19.06%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,29 +1,9 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        # adj = defaultdict(list)
         fromset = set()
-        toset = set()
-        for i, j in paths:
-            fromset.add(i)
-            toset.add(j)
+        for p in paths:
+            fromset.add(p[0])
             
-        newset = toset.difference(fromset)
-        
-        # print(fromset)
-        # print(toset)
-        # print(newset, newset.difference(fromset).pop())
-        return newset.difference(fromset).pop()
-#             # print(i,j)
-#             if adj.get(i,[]) == []:
-#                 adj[i].append(j)
-#             else:
-#                 adj[i].append(j)
-        
-#         visit = set()
-#         s = set(adj.keys())
-        
-#         for i in adj:
-#             visit.add(adj[i])
-            
-            
-        # print(adj)
+        for p in paths:
+            if p[1] not in fromset:
+                return p[1]
