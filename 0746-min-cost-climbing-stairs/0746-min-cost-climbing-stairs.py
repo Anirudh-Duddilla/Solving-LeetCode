bc1991d5f9faffa41590259d4e76fd5403bde54a
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-12-17 15:11:06 -0800

    Time: 3 ms (73.26%), Space: 17.7 MB (17.98%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,26 +1,10 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        l = len(cost)
+        min_cost = [0]*len(cost)
+        min_cost[0] = cost[0]
+        min_cost[1] = cost[1]
         
-        # If there are only 2 steps, return the min of the first two
-        if l == 2:
-            return min(cost[0], cost[1])
+        for i in range(2, len(cost)):
+            min_cost[i] = min(min_cost[i-1], min_cost[i-2])+cost[i]
         
-        # Initialize the result array with -1 for memoization
-        res_cost = [-1] * l
-        
-        # Base cases for the first two steps
-        res_cost[0] = cost[0]
-        res_cost[1] = cost[1]
-        
-        def recurse(n):
-            # Return memoized result if already computed
-            if res_cost[n] != -1:
-                return res_cost[n]
-            
-            # Recursively calculate the cost for n-th step
-            res_cost[n] = min(recurse(n-2) + cost[n], recurse(n-1) + cost[n])
-            return res_cost[n]
-        
-        # Compute the result for the last step
-        return min(recurse(l-1), recurse(l-2))
+        return min(min_cost[-2], min_cost[-1])
