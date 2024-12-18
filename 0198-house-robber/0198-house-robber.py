7a466fb050796eae7415806ba4983f550b17ec45
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2024-12-17 21:43:44 -0800

    Time: 0 ms (100.00%), Space: 17.9 MB (6.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,23 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        
         l = len(nums)
-        res_cost = [-1] * l
+        if l == 1:
+            return nums[0]
         
+        # Initialize the DP array
+        res_cost = [0] * l
         res_cost[0] = nums[0]
-        if l > 1:
-            res_cost[1] = max(nums[0],nums[1])
+        res_cost[1] = max(nums[0], nums[1])
         
-        def recurse(n):
+        for i in range(2, l):
+            res_cost[i] = max(res_cost[i - 2] + nums[i], res_cost[i - 1])
             
-            if res_cost[n] == -1:
-                # print(n,n-2,n-1,nums[n-2], nums[n-1])
-                res_cost[n] = max(recurse(n-2)+nums[n],recurse(n-1))
-                # print(n,n-2,n-1)
-                # print(res_cost[n], res_cost[n-2],res_cost[n-1],res_cost)
-            return res_cost[n]
-                
-        recurse(l-1)
         
-        # print(res_cost)
         
         return res_cost[-1]
