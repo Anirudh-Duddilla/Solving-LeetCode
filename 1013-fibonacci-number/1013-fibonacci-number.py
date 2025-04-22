efe03e68f06e72f5ccd84f80dd7720f2c84969bc
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2025-04-22 14:10:25 -0700

    1013-FIBONACCI-NUMBER Stats: Time: 40 ms (59.13%), Space: 17.6 MB (89.56%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -2,14 +2,9 @@ class Solution:
     def fib(self, n: int) -> int:
 
         a, b = 0, 1
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            res = [0] * (n+1)
-            res[0], res[1] = a, b
-            for i in range(2, n+1):
-                res[i] = res[i -1] + res[i-2]
-            return res[n]
+        
+        for i in range(2, n+1):
+            a, b = b, a+b
+
+        return b if n!=0 else a
         
