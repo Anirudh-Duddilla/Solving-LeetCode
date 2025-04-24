eda65b067a7697be3ba3d86b70aef8464bd5f293
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2025-04-24 12:31:22 -0700

    0213-HOUSE-ROBBER-II Stats: Time: 0 ms (100%), Space: 17.6 MB (85.95%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,29 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
         if len(nums) == 1:
-            return nums[-1]
-        elif len(nums) == 2:
-            return max(nums[-1], nums[-2])
-        else:
+            return nums[0]
 
-            res1 = [0] * (n-1)
-            res1[0], res1[1] = nums[0], max(nums[0],nums[1])
 
-            for i in range(2,n-1):
+        def simple_rob(nums):
+            n = len(nums)
+            print(nums)
+            if len(nums) == 1:
+                return nums[0]
+            prev, curr = nums[0], max(nums[0], nums[1])
 
-                res1[i] = max(res1[i-1], res1[i-2]+ nums[i])
-
-            # print("r1",res1)
-
-            res2 = [0] * (n)
-            res2[1], res2[2] = nums[1], max(nums[1],nums[2])
-            # print("r2", res2)
             for i in range(2,n):
-                # print("i", i, "n[i]", nums[i], "r2[i]", res2[i])
-                # print("r2[i-1]", res2[i-1],"r2[i-2]", res2[i-2], "n[i]", nums[i])
-                res2[i] = max(res2[i-1], res2[i-2]+ nums[i])
-                # print("r2", res2)
 
+                prev, curr = curr, max(curr, prev + nums[i])
 
-        return max(res1[-1], res2[-1])
+            return curr
+
+        return max(simple_rob(nums[:-1]), simple_rob(nums[1:]))
