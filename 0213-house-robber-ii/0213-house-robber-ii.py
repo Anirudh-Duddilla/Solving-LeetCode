20163b6cd7e5c1c9156f8a588b41b7133ceddf8b
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2025-04-24 12:25:32 -0700

    0213-HOUSE-ROBBER-II Stats: Time: 3 ms (10.56%), Space: 17.8 MB (58.19%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -10,7 +10,7 @@ class Solution:
             res1 = [0] * (n-1)
             res1[0], res1[1] = nums[0], max(nums[0],nums[1])
 
-            for i in range(2,len(nums)-1):
+            for i in range(2,n-1):
 
                 res1[i] = max(res1[i-1], res1[i-2]+ nums[i])
 
@@ -18,11 +18,12 @@ class Solution:
 
             res2 = [0] * (n)
             res2[1], res2[2] = nums[1], max(nums[1],nums[2])
-            for i in range(3,len(nums)):
+            # print("r2", res2)
+            for i in range(2,n):
                 # print("i", i, "n[i]", nums[i], "r2[i]", res2[i])
                 # print("r2[i-1]", res2[i-1],"r2[i-2]", res2[i-2], "n[i]", nums[i])
                 res2[i] = max(res2[i-1], res2[i-2]+ nums[i])
-            # print("r2", res2)
+                # print("r2", res2)
 
 
         return max(res1[-1], res2[-1])
