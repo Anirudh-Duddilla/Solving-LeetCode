b179aae14f47c9349f0e1b74728e8a8769546c65
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2025-04-25 15:00:58 -0700

    0647-PALINDROMIC-SUBSTRINGS Stats: Time: 391 ms (14.53%), Space: 219 MB (5.04%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,40 +1,33 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         sl = list(s)
-        # res = []
-        reslen = 0
+        res = []
         def pal(i,j, n, s1):
-            # r = []
-            rl = 0
+            r = []
             while i >=0 and j < n and s1[i] == s1[j]:
                 if j - i >= 0:
                     # print("eif", i, j+1, s1[i:j+1])
-                    # r.append(s1[i:j+1])
-                    rl+=1
+                    r.append(s1[i:j+1])
                 i, j = i-1, j+1
                 
             # print("s1",s1,"r",r)
-            return rl
+            return r
 
         def palsubstr(s):
             n = len(s)
-            reslen = 0
             for i in range(0, n):
                 odd = pal(i,i,n,s)
                 if odd !="":
-                    # res.extend(odd)
-                    reslen+=odd
-                # print("reso", reslen)
+                    res.extend(odd)
+                # print("reso", res)
                 # print("o", odd, "i",i)
                 if (i+1) < n and s[i] == s[i+1]:
                     even = pal(i,i+1,n,s)
                     if even !="":
-                        # res.extend(even)
-                        reslen+=even
-                    # print("e", even,"i",i,"i+1",i+1)
-                    # print("rese", reslen)
-            return reslen
-        reslen = palsubstr(s)
+                        res.extend(even)
+                # print("e", even,"i",i,"i+1",i+1)
+                    # print("rese", res)
+        palsubstr(s)
         # print(res)
-        return reslen
+        return len(res)
             
