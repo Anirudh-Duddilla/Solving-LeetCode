dce9a94ee969fdd05cd32b1fb986b02fbb3e4adb
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2025-04-25 10:04:33 -0700

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 267 ms (71.51%), Space: 18.1 MB (29.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,22 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        reslen = 0
-        res = ""
+        if s == "":
+            return ""
 
-        for i in range(len(s)):
-            j,k = i,i
-            while j >= 0 and k < len(s) and s[j] == s[k]:
-                if (k-j+1) > reslen:
-                    res = s[j:k+1]
-                    reslen = k-j+1
-                j-=1
-                k+=1
-            j,k = i,i+1
-            while j >= 0 and k < len(s) and s[j] == s[k]:
-                if (k-j+1) > reslen:
-                    res = s[j:k+1]
-                    reslen = k-j+1
-                j-=1
-                k+=1
 
-        return res
+        def trav(i,j):
+            while i >=0 and j < len(s) and s[i] == s[j]:
+                i, j = i-1, j+1
+            # print(s[i+1:j])
+            return s[i+1:j]
+
+        longest = ""
+        for i in range(0,len(s)):
+            odd = trav(i,i)
+            if len(odd) > len(longest):
+                longest = odd
+
+        # for i in range(0,len(s)):
+            even = trav(i, i+1)
+            if len(even) > len(longest):
+                longest = even
+
+            
+
+        return longest
