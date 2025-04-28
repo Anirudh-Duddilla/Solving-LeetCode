f70ee14e153d799db316bdf515594828096aac43
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2025-04-28 15:19:57 -0700

    0091-DECODE-WAYS Stats: Time: 3 ms (27.2%), Space: 18.4 MB (9.65%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,52 +1,34 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        n = len(s)
-        res = 0
-        r = []
-        if s[0] == "0":
-            return res
-        
-        memo = {}
-        def bt(sl):
+        if not s or s[0] == "0":
+            return 0
 
+        memo = {}
+
+        def bt(sl):
             if sl in memo:
                 return memo[sl]
             if sl == "":
-                return  1
-            elif len(sl) == 1:
+                return 1
+            if sl[0] == "0":
+                return 0
+            if len(sl) == 1:
                 memo[sl] = 1
                 return 1
-            elif len(sl) == 2:
-                s0, s1 = int(sl[0]), int(sl[1])
-                prod = (s0*10) + s1
-                if s0 == 0 or (s1 == 0 and prod > 20):
-                    memo[sl] = 0
-                    return 0
-                if prod < 27 and s1 != 0:
-                    memo[sl] = 2
-                    return 2
-                elif prod < 27 and s1 == 0:
-                    memo[sl] = 1
-                    return 1
-                else:
-                    memo[sl] = 1
-                    return 1
-            else:
-                s0, s1 = int(sl[-2]), int(sl[-1])
-                prod = (s0 * 10) + s1
-                if s1 == 0:
-                    if 1<=s0<=2:
-                        memo[sl] = bt(sl[0:-2])
-                    else:
-                        memo[sl] = 0
-                elif prod < 27 and s0 != 0:
-                    memo[sl] = bt(sl[0:-1]) + bt(sl[0:-2])
-                else:
-                    memo[sl] = bt(sl[0:-1])
-                
-                print("m",memo)
-                return memo[sl]
 
-        res = bt(s)
+            count = 0
 
-        return res
+            # Single digit (1â9)
+            if 1 <= int(sl[-1]) <= 9:
+                count += bt(sl[:-1])
+
+            # Two digits (10â26)
+            if len(sl) >= 2:
+                two_digit = int(sl[-2:])
+                if 10 <= two_digit <= 26:
+                    count += bt(sl[:-2])
+
+            memo[sl] = count
+            return count
+
+        return bt(s)
