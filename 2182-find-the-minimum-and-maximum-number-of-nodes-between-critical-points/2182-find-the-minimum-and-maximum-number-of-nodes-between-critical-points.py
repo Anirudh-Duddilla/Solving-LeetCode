8999333ba1f15ad060e239b85ad3398874a487c9
Anirudh-Duddilla <65510725+Anirudh-Duddilla@users.noreply.github.com>
2025-05-05 17:46:46 -0700

    2182-FIND-THE-MINIMUM-AND-MAXIMUM-NUMBER-OF-NODES-BETWEEN-CRITICAL-POINTS Stats: Time: 109 ms (28.04%), Space: 47.6 MB (93.93%) - LeetHub

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        mind, maxd = inf, -1
+
+        prev, curr = head, head.next
+        i, j, k = 0, 0, 1
+        while curr.next:
+
+            if prev.val > curr.val < curr.next.val or prev.val < curr.val > curr.next.val:
+                # print("i", i, "j", j, "k", k)
+                # print("mind", mind)
+                # print("maxd", maxd)
+                i = k if i == 0 else i
+                maxd = max(maxd, k - i) if maxd <= 0 else k - i
+                if j != 0:
+                    mind = min(mind, k - j) if mind != inf else k - j
+
+                j = k
+            prev = curr
+            curr = curr.next
+            k += 1
+
+        mind = mind if mind != inf else -1
+        maxd = maxd if maxd > 0 else -1
+
+        return [mind,maxd]
--- README.md
+++ README.md
@@ -156,6 +156,7 @@ Collection of LeetCode questions to ace the coding interview! - Created using [L
 ## Linked List
 |  |
 | ------- |
+| [2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points](https://github.com/Anirudh-Duddilla/Solving-LeetCode/tree/master/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points) |
 | [2299-merge-nodes-in-between-zeros](https://github.com/Anirudh-Duddilla/Solving-LeetCode/tree/master/2299-merge-nodes-in-between-zeros) |
 ## Simulation
 |  |
